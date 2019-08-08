b1c633dd63debe02b643da7e652da2bf07b2438f
tejas12356 <31923027+tejas12356@users.noreply.github.com>
2019-08-08 18:52:41 +0530

    Update FizzBuzz.java

--- src/main/java/com/techprimers/testing/FizzBuzz.java
+++ src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -1,14 +1,15 @@
-package com.techprimers.testing;
-
-public class FizzBuzz {
-
-    public String play(int number) {
-
-        if (number == 0) throw new IllegalArgumentException("Number must not be 0");
-        if (number % 3 == 0) return "Fizz";
-        if (number % 5 == 0) return "Buzz";
-
-
-        return String.valueOf(number);
-    }
-}
+import java.util.*;  
+public class Lambda{  
+    public static void main(String[] args) {  
+          
+        List<String> list=new ArrayList<String>();  
+        list.add("ankit");  
+        list.add("mayank");  
+        list.add("irfan");  
+        list.add("jai");  
+          
+        list.forEach(  
+            (n)->System.out.println(n)  
+        );  
+    }  
+}  
