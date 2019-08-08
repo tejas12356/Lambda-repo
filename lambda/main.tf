b94077ecbf349ae0d862b70e8f82468eaa1dfd53
Tejas Patil1 <Tejas.Patil1@synechron.com>
2019-08-08 15:17:22 +0530

    changes in config

--- lambda/main.tf
+++ lambda/main.tf
@@ -1,6 +1,6 @@
 resource "aws_lambda_function" "lambda" {
   s3_bucket = "eqbucket34"
-  s3_key    = "artifact/maven.zip"
+  s3_key    = "artifact/jenkins-example-1.0-SNAPSHOT.jar"
   function_name = "${var.name}_${var.handler}"
   role          = "${var.role}"
   handler       = "${var.name}.${var.handler}"
--- main.tf
+++ main.tf
@@ -25,66 +25,66 @@ resource "aws_iam_role" "iam_role_for_lambda" {
 EOF
 }
 
-# Here is a first lambda function that will run the code `hello_lambda.handler`
+# Here is a first lambda function that will run the code `eq_lambda.handler`
 module "lambda" {
   source  = "./lambda"
-  name    = "hello_lambda"
+  name    = "eq_test_lambda"
   runtime = "java8"
   role    = "${aws_iam_role.iam_role_for_lambda.arn}"
 }
 
 # This is a second lambda function that will run the code
-# `hello_lambda.post_handler`
+# `eq_lambda.post_handler`
 module "lambda_post" {
   source  = "./lambda"
-  name    = "hello_lambda"
+  name    = "eq_test_lambda"
   handler = "post_handler"
   runtime = "java8"
   role    = "${aws_iam_role.iam_role_for_lambda.arn}"
 }
 
 # Now, we need an API to expose those functions publicly
-resource "aws_api_gateway_rest_api" "hello_api" {
-  name = "Hello API"
+resource "aws_api_gateway_rest_api" "eq_api" {
+  name = "eq API"
 }
 
 # The API requires at least one "endpoint", or "resource" in AWS terminology.
-# The endpoint created here is: /hello
-resource "aws_api_gateway_resource" "hello_api_res_hello" {
-  rest_api_id = "${aws_api_gateway_rest_api.hello_api.id}"
-  parent_id   = "${aws_api_gateway_rest_api.hello_api.root_resource_id}"
-  path_part   = "hello"
+# The endpoint created here is: /eq
+resource "aws_api_gateway_resource" "eq_api_res_eq" {
+  rest_api_id = "${aws_api_gateway_rest_api.eq_api.id}"
+  parent_id   = "${aws_api_gateway_rest_api.eq_api.root_resource_id}"
+  path_part   = "eq"
 }
 
 # Until now, the resource created could not respond to anything. We must set up
 # a HTTP method (or verb) for that!
-# This is the code for method GET /hello, that will talk to the first lambda
-module "hello_get" {
+# This is the code for method GET /eq, that will talk to the first lambda
+module "eq_get" {
   source      = "./api_method"
-  rest_api_id = "${aws_api_gateway_rest_api.hello_api.id}"
-  resource_id = "${aws_api_gateway_resource.hello_api_res_hello.id}"
+  rest_api_id = "${aws_api_gateway_rest_api.eq_api.id}"
+  resource_id = "${aws_api_gateway_resource.eq_api_res_eq.id}"
   method      = "GET"
-  path        = "${aws_api_gateway_resource.hello_api_res_hello.path}"
+  path        = "${aws_api_gateway_resource.eq_api_res_eq.path}"
   lambda      = "${module.lambda.name}"
   region      = "${var.aws_region}"
   account_id  = "${data.aws_caller_identity.current.account_id}"
 }
 
-# This is the code for method POST /hello, that will talk to the second lambda
-module "hello_post" {
+# This is the code for method POST /eq, that will talk to the second lambda
+module "eq_post" {
   source      = "./api_method"
-  rest_api_id = "${aws_api_gateway_rest_api.hello_api.id}"
-  resource_id = "${aws_api_gateway_resource.hello_api_res_hello.id}"
+  rest_api_id = "${aws_api_gateway_rest_api.eq_api.id}"
+  resource_id = "${aws_api_gateway_resource.eq_api_res_eq.id}"
   method      = "POST"
-  path        = "${aws_api_gateway_resource.hello_api_res_hello.path}"
+  path        = "${aws_api_gateway_resource.eq_api_res_eq.path}"
   lambda      = "${module.lambda_post.name}"
   region      = "${var.aws_region}"
   account_id  = "${data.aws_caller_identity.current.account_id}"
 }
 
 # We can deploy the API now! (i.e. make it publicly available)
-resource "aws_api_gateway_deployment" "hello_api_deployment" {
-  rest_api_id = "${aws_api_gateway_rest_api.hello_api.id}"
+resource "aws_api_gateway_deployment" "eq_api_deployment" {
+  rest_api_id = "${aws_api_gateway_rest_api.eq_api.id}"
   stage_name  = "production"
-  description = "Deploy methods: ${module.hello_get.http_method} ${module.hello_post.http_method}"
+  description = "Deploy methods: ${module.eq_get.http_method} ${module.eq_post.http_method}"
 }
