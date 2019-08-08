2a9ff647963723081c8caad7bd4cc5957e826578
tejas12356 <31923027+tejas12356@users.noreply.github.com>
2019-08-08 19:03:13 +0530

    Update FizzBuzz.java

--- src/main/java/com/techprimers/testing/FizzBuzz.java
+++ src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -7,3 +7,8 @@ public class FizzBuzz {
         if (number == 0) throw new IllegalArgumentException("Number must not be 0");
         if (number % 3 == 0) return "Fizz";
         if (number % 5 == 0) return "Buzz";
+
+
+        return String.valueOf(number);
+    }
+}
