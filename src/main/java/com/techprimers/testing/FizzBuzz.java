97b0f44829e32c01e477c927b97a6f9f8406779e
tejas12356 <31923027+tejas12356@users.noreply.github.com>
2019-08-08 18:56:49 +0530

    Update FizzBuzz.java

--- src/main/java/com/techprimers/testing/FizzBuzz.java
+++ src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -1,14 +1,14 @@
-package com.techprimers.testing;
+@FunctionalInterface
+interface MyFunctionalInterface {
 
-public class FizzBuzz {
+	//A method with single parameter
+    public int incrementByFive(int a);
+}
+public class Example {
 
-    public String play(int number) {
-
-        if (number == 0) throw new IllegalArgumentException("Number must not be 0");
-        if (number % 3 == 0) return "Fizz";
-        if (number % 5 == 0) return "Buzz";
-
-
-        return String.valueOf(number);
+   public static void main(String args[]) {
+        // lambda expression with single parameter num
+    	MyFunctionalInterface f = (num) -> num+5;
+        System.out.println(f.incrementByFive(22));
     }
 }
